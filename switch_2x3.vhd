238641349bbc75fe3bcd187bef2636383141b136
bdzanko17 <bdzanko1@gmail.com>
2019-05-16 20:41:48 +0200

    best EU

--- switch_2x3.vhd
+++ switch_2x3.vhd
@@ -14,23 +14,25 @@ PORT	 (
 END switch_2x3;
 
 ARCHITECTURE arch_switch_2x3 OF switch_2x3 IS 
+signal HighImpedance : std_logic_vector(7 downto 0)  := "ZZZZZZZZ";
+
 BEGIN
 	With S select U <=
 		x when "000",
 		x when "001",
 		y when "010",
 		y when "100",
-		"ZZZZZZZZ" when others;
+		HighImpedance when others;
 	With S select V <=
 		x when "010",
 		x when "011",
 		y when "000",
 		y when "101",
-		"ZZZZZZZZ" when others;
+		HighImpedance when others;
 	With S select R <=
 		x when "100",
 		x when "101",
 		y when "001",
 		y when "011",
-		"ZZZZZZZZ" when others;
+		HighImpedance when others;
 END ARCHITECTURE;
